365b835b2bb293aba088a361c0a33753ef687b58
pedroyasui <pedro.yasui@bitzen.com.br>
2021-10-13 13:32:42 -0300

    criação da tela de fabricantes

---
+++ 2021-net-turma1/Pedro/TreinamentoBitzen/Controllers/ManufacturerController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using TreinamentoBitzen.RegrasNegocio.Services;
+using TreinamentoBitzen.ViewModels;
+
+namespace TreinamentoBitzen.Controllers
+{
+    public class ManufacturerController : Controller
+    {
+        private readonly ManufacturerService _manufacturerService = null;
+
+        public ManufacturerController()
+        {
+            _manufacturerService = new ManufacturerService();
+        }
+
+        #region Actions
+
+        #region Index
+        [HttpGet]
+        public ActionResult Index()
+        {
+            var listEntities = _manufacturerService.List();
+            var listViewModels = new List<ManufacterViewModel>();
+
+            // Mapeamento Model X ViewModel
+#warning Desafio #1 - Refazer este for com o comando .Select (Linq)
+            foreach (var manufacter in listEntities)
+            {
+                // Opção 1
+                var viewModel = new ManufacterViewModel();
+                viewModel.Id = manufacter.Id;
+                viewModel.Name = manufacter.Name;
+                viewModel.Active = manufacter.Active;
+
+                listViewModels.Add(viewModel);
+
+                //// Opção 2 - Simplificar a inicialização do objeto
+                //var viewModel2 = new ManufacterViewModel
+                //{
+                //    Id = manufacter.Id,
+                //    Name = manufacter.Name,
+                //    Active = manufacter.Active
+                //};
+                //listViewModels.Add(viewModel2);
+            }
+
+            // Preencher os dados na View
+            return View(listViewModels);
+        }
+        #endregion
+
+        #endregion
+    }
+}
