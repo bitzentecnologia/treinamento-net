f0c86aa5fdd08afefe382ccfcd866a097b303eff
Djakson Cleber <djakson.filho@gmail.com>
2022-01-05 15:08:41 -0300

    Adds Create Vehicle

--- 2021-net-turma1/Djakson/TreinamentoBitzen/Controllers/VehicleController.cs
+++ 2021-net-turma1/Djakson/TreinamentoBitzen/Controllers/VehicleController.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Linq;
 using System.Web.Mvc;
+using TreinamentoBitzen.Dados.Entidades;
 using TreinamentoBitzen.RegrasNegocio.Services;
 using TreinamentoBitzen.ViewModels;
 
@@ -27,15 +25,17 @@ namespace TreinamentoBitzen.Controllers
         {
             var listEntities = _vehicleService.List();
 
+            // Mapeamento Model X ViewModel
+
             var listModel = listEntities.Select(x => new VehicleViewModel
             {
                 Id = x.Id,
                 Name = x.Name,
                 Active = x.Active,
                 LicensePlate = x.LicensePlate,
-                FuelType = x.FuelType,
+                //FuelType = x.FuelType,
                 FuelTypesId = x.FuelTypesId,
-                Manufacturer = x.Manufacturer,
+                //Manufacturer = x.Manufacturer,
                 ManufacturersId = x.ManufacturersId,
                 ManufactureYear = x.ManufactureYear,
                 Remarks = x.Remarks,
@@ -45,6 +45,60 @@ namespace TreinamentoBitzen.Controllers
             return View(listModel);
         }
 
+        [HttpGet]
+        public ActionResult Details(int id)
+        {
+            var detail = _vehicleService.GetById(id);
+            var viewModel = new VehicleViewModel
+            {
+                Id = id,
+                Name = detail.Name,
+                Active = detail.Active,
+                //FuelType = detail.FuelType,
+                FuelTypesId = detail.FuelTypesId,
+                LicensePlate = detail.LicensePlate,
+                //Manufacturer = detail.Manufacturer,
+                ManufacturersId = detail.ManufacturersId,
+                ManufactureYear = detail.ManufactureYear,
+                Remarks = detail.Remarks,
+                TankCapacity = detail.TankCapacity
+            };
+
+            return View(viewModel);
+        }
+
+        // GET - CREATE
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST - CREATE
+        [HttpPost]
+        public ActionResult Create(VehicleViewModel viewModel)
+        {
+            if (viewModel != null && viewModel.Name != null)
+            {
+                var newModel = new Vehicle
+                {
+                    Name = viewModel.Name,
+                    Active = viewModel.Active,
+                    FuelType = viewModel.FuelType,
+                    FuelTypesId = viewModel.FuelTypesId,
+                    Id = viewModel.Id,
+                    LicensePlate = viewModel.LicensePlate,
+                    Manufacturer = viewModel.Manufacturer,
+                    ManufacturersId = viewModel.ManufacturersId,
+                    ManufactureYear = viewModel.ManufactureYear,
+                    Remarks = viewModel.Remarks,
+                    TankCapacity = viewModel.TankCapacity
+                };
+                _vehicleService.Save(newModel);
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
         #endregion
 
         #endregion
